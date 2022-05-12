097425b18447eddf77b29d7fd07ba7fede62af41
Diego Souza <diegosouzagestor@gmail.com>
2022-05-12 16:50:27 -0300

    Commit changes config new project of ignite

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
-import react from "react";
+import { render } from "react-dom";
+import { App } from "./App";
 
-function App() {
-    const name = <h1>hello World!</h1>;
-}
+render(<App/>, document.getElementById('root'))
